250d35f8a2a866914c01c82ee43334422cdf85a2
MurakamiShun <enshuuritsu.about3@gmail.com>
2024-02-18 03:07:49 +0900

    update test source

--- test/build_tests.sh
+++ test/build_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-FILE_NAME="${$1%.*}"
-riscv64-unknown-elf-objcopy -O binary $1 $FILE_NAME.bin
-od -An -tx4 -w4 -v $FILE_NAME.bin > $FILE_NAME.hex
+#FILE_NAME="${$1%.*}"
+riscv32-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -nostartfiles -mcmodel=medany -fvisibility=hidden -static -c testprog.c -o testprog.o
+riscv32-unknown-elf-ld testprog.o -T link.ld -static -o testprog.elf
+riscv32-unknown-elf-objdump -D testprog.elf > testprog.dump
+riscv32-unknown-elf-objcopy -O binary testprog.elf testprog.bin
+od -An -tx4 -w4 -v testprog.bin > testprog.hex
---
+++ test/testprog.c
@@ -0,0 +1,11 @@
+extern unsigned char _input[];
+extern unsigned char _output[];
+
+
+__attribute__((naked))
+__attribute__((section(".text.entry")))
+void entry(void){
+    for(int i = 0; i < 32; ++i){
+        *_output = i + '0';
+    }
+}
